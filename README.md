0ef6f3d42559396665c52ffde277e8759d36eef5
hi-deen <yoursmilingdoctor@gmail.com>
2025-11-17 23:18:22 +0100

    Prepared for deployment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # anonymous-message
 A web application that allows registered users to create a message link where they can receive anonymous messages from people.
+
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
-import connectMongodb from "./db/connectMongodb.js";
 import { validateInput } from "./middleware/authMiddleware.js";
+
+import connectMongodb from "./db/connectMongodb.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -20,9 +24,28 @@ app.use("/api/users", validateInput);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectMongodb();
-}) 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-console.log("Server setup complete");
+  // serve index.html for any unmatched route without using a path pattern
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//     connectMongodb();
+// });
+
+(async () => {
+  try {
+    await connectMongodb();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to DB, exiting", err);
+    process.exit(1);
+  }
+})();
